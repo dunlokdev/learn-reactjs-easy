71018a8c5f41de278c92d329e2a99e073f3512eb
dunlok <myloc442@gmail.com>
2023-03-19 20:35:56 +0700

    50. Sự khác biệt giữa Route và Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink, Route } from "react-router-dom";
+import { Link, NavLink, Route, Switch } from "react-router-dom";
 import TodoFeature from "./features/Todo";
 import AlbumFeature from "./features/Album";
 
@@ -7,20 +7,21 @@ function App() {
   return (
     <div className="App">
       <h1>Header</h1>
-      <div>
-        <Link to={"/todos"}>Todos</Link>
-      </div>
-      <div>
-        <Link to={"/albums"}>Albums</Link>
-      </div>
-      <div>
-        <NavLink to={"/todos"} activeClassName="active-todo">Todos</NavLink>
-      </div>
-      <div>
-        <NavLink to={"/albums"}>Albums</NavLink>
-      </div>
+      <NavLink to={"/todos"} activeClassName="active-todo">Todos</NavLink>
+      <br />
+      <NavLink to={"/albums"}>Albums</NavLink>
+
+      {/* Không sử dụng Switch */}
+      {/* <Route path={"/"} component={TodoFeature} />
       <Route path={"/todos"} component={TodoFeature} />
-      <Route path={"/albums"} component={AlbumFeature} />
+      <Route path={"/albums"} component={AlbumFeature} /> */}
+
+      <Switch>
+        <Route path={"/"} component={TodoFeature} />
+        <Route path={"/todos"} component={TodoFeature} />
+        <Route path={"/albums"} component={AlbumFeature} />
+      </Switch>
+
       <h1>Footer</h1>
     </div>
   );
