499b22d64ea17af8bb18708ab244b2bb816f9276
dunlok <myloc442@gmail.com>
2023-03-19 12:56:25 +0700

    Source base

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ColorBox from "./components/ColorBox";
+import Counter from "./components/Counter";
+import AlbumFeature from "./features/Album";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AlbumFeature /> */}
+      <ColorBox />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
