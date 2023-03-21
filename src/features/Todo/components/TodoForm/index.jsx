0dbeed6c337d26344a582f2f1fe6c71da103c6fb
dunlok <myloc442@gmail.com>
2023-03-21 21:30:46 +0700

    83. Xử lý relative import với jsconfig.json

--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -1,26 +1,26 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import InputField from 'components/form-controls/InputField';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import * as yup from "yup";
-import InputField from '../../../../components/form-controls/InputField';
+import * as yup from 'yup';
+// import InputField from '../../../../components/form-controls/InputField';
 
 TodoForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
-
   const schema = yup.object().shape({
-    title: yup.string()
+    title: yup
+      .string()
       .min(2, 'Title is too short')
       .required('Please enter title'),
-
   });
 
   const form = useForm({
     defaultValues: {
-      title: '' // Phải liệt kê các defualt value ở đây nếu không form sẽ lỗi vì k nhận biết được
+      title: '', // Phải liệt kê các defualt value ở đây nếu không form sẽ lỗi vì k nhận biết được
     },
     resolver: yupResolver(schema),
   });
@@ -33,11 +33,11 @@ function TodoForm(props) {
     }
 
     form.reset();
-  }
+  };
 
   return (
     <form onSubmit={form.handleSubmit(handleSubmit)}>
-      <InputField name="title" label="Todo" form={form} />
+      <InputField name='title' label='Todo' form={form} />
     </form>
   );
 }
