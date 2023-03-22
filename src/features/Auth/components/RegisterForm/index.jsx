509e11340e528af7d1dfc27151ded4c5359df565
dunlok <myloc442@gmail.com>
2023-03-22 22:13:03 +0700

    96. Style cho form đăng ký

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import InputField from 'components/form-controls/InputField';
 import PropTypes from 'prop-types';
@@ -11,7 +11,28 @@ RegisterForm.propTypes = {
   onSubmit: PropTypes.func,
 };
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(4),
+  },
+
+  avatar: {
+    margin: '0 auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
+
+  title: {
+    textAlign: 'center',
+    margin: theme.spacing(2, 0, 3, 0),
+  },
+
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 function RegisterForm(props) {
+  const classes = useStyles();
   const schema = yup.object().shape({
     title: yup
       .string()
@@ -40,12 +61,12 @@ function RegisterForm(props) {
   };
 
   return (
-    <div>
-      <Avatar>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
         <LockOutlined></LockOutlined>
       </Avatar>
 
-      <Typography component='h3' variant='h5'>
+      <Typography className={classes.title} component='h3' variant='h5'>
         Create an account
       </Typography>
 
@@ -55,6 +76,15 @@ function RegisterForm(props) {
         <InputField name='email' label='Email' form={form} />
         <InputField name='password' label='Password' form={form} />
         <InputField name='retypePassword' label='Retype password' form={form} />
+
+        <Button
+          variant='contained'
+          color='primary'
+          fullWidth
+          className={classes.submit}
+        >
+          Create an account
+        </Button>
       </form>
     </div>
   );
