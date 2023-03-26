84e821258287de250310568670683f5528fccea7
dunlok <myloc442@gmail.com>
2023-03-26 22:49:06 +0700

    Update syntax createAsyncThunk

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import userApi from 'api/userApi';
 
+// First, create the thunk
 export const register = createAsyncThunk(
   'user/register',
   // muốn dispatch một action thì dùng thunkAPI
@@ -17,6 +18,7 @@ export const register = createAsyncThunk(
   }
 );
 
+// Then, handle actions in your reducers:
 const userSlice = createSlice({
   name: 'user',
   initialState: {
@@ -24,11 +26,20 @@ const userSlice = createSlice({
     settings: {}, // thông tin phụ
   },
 
-  reducer: {},
-  extraReducers: {
-    [register.fulfilled]: (state, action) => {
+  reducer: {
+    // standard reducer logic, with auto-generated action types per reducer
+  },
+  // extraReducers: {
+  //   [register.fulfilled]: (state, action) => {
+  //     state.current = action.payload; // action.payload sẽ lấy data từ return result func Register
+  //   },
+
+  // },
+  extraReducers: (builder) => {
+    // Add reducers for additional action types here, and handle loading state as needed
+    builder.addCase(register.fulfilled, (state, action) => {
       state.current = action.payload; // action.payload sẽ lấy data từ return result func Register
-    },
+    });
   },
 });
 
