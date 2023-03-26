6946106408e8ca38c8bcb5f9a2c49024f4fccbc0
dunlok <myloc442@gmail.com>
2023-03-26 22:20:23 +0700

    111. Thêm close icon vào dialog

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import { Link, NavLink } from 'react-router-dom';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Close } from '@material-ui/icons';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import Register from 'features/Auth/components/Register';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,6 +26,13 @@ const useStyles = makeStyles((theme) => ({
   link: {
     color: '#fff',
     textDecoration: 'none',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: theme.spacing(1),
+    right: theme.spacing(1),
+    color: theme.palette.grey[500],
+    zIndex: 1,
   },
 }));
 
@@ -73,16 +81,14 @@ export default function Header() {
         aria-labelledby='form-dialog-title'
         disableEscapeKeyDown
       >
+        <IconButton className={classes.closeButton} onClick={handleClose}>
+          <Close />
+        </IconButton>
         <DialogContent>
           <DialogContentText>
             <Register closeDialog={handleClose} />
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color='primary'>
-            Cancel
-          </Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
