659edf34709a0ec139bf0980a5f4868f03fd1fb8
dunlok <myloc442@gmail.com>
2023-03-26 22:53:26 +0700

    112. Setup API đăng nhập

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -6,9 +6,23 @@ export const register = createAsyncThunk(
   'user/register',
   // muốn dispatch một action thì dùng thunkAPI
   async (payload) => {
-    console.log(payload);
     // call api to register
     const data = await userApi.register(payload);
+
+    // save data to local storage
+    localStorage.setItem('access_token', data.jwt);
+    localStorage.setItem('user', JSON.stringify(data.user));
+
+    return data.user;
+  }
+);
+
+export const login = createAsyncThunk(
+  'user/login',
+  // muốn dispatch một action thì dùng thunkAPI
+  async (payload) => {
+    // call api to register
+    const data = await userApi.login(payload);
 
     // save data to local storage
     localStorage.setItem('access_token', data.jwt);
@@ -39,6 +53,10 @@ const userSlice = createSlice({
     // Add reducers for additional action types here, and handle loading state as needed
     builder.addCase(register.fulfilled, (state, action) => {
       state.current = action.payload; // action.payload sẽ lấy data từ return result func Register
+    });
+
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.current = action.payload; // action.payload sẽ lấy data từ return result func Login
     });
   },
 });
