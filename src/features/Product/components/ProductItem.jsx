9afe8077e081ac72fcfc414b5ccae24803c3b5c0
dunlok <myloc442@gmail.com>
2023-03-28 22:32:59 +0700

    127. Xử lý thumbnail cho sản phẩm

--- src/features/Product/components/ProductItem.jsx
+++ src/features/Product/components/ProductItem.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
-import { Skeleton } from '@material-ui/lab';
+import { STATIC_HOST, THUMBNAIL_PLACEHODLER } from 'constrants/index';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 ProductItem.propTypes = {
   product: PropTypes.object,
 };
 
 function ProductItem({ product }) {
+  const thumbnailUrl = product.thumbnail
+    ? `${STATIC_HOST}${product.thumbnail?.url}`
+    : `${THUMBNAIL_PLACEHODLER}`;
+
   return (
     <Box padding={1}>
-      <Skeleton variant='rect' width='100%' height={118} />
+      <Box padding={1}>
+        <img src={thumbnailUrl} alt={product.name} width='100%' height={183} />
+      </Box>
       <Typography variant='body2'>{product.name}</Typography>
       <Typography variant='body2'>
         {product.salePrice} - {product.promotionPercent}%
