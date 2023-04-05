47c3ac55e15355fa3a565c486b613a4519524bd9
dunlok <myloc442@gmail.com>
2023-04-05 23:20:00 +0700

    135. Lọc theo khoảng giá

--- src/features/Product/components/Filters/FilterByPrice.jsx
+++ src/features/Product/components/Filters/FilterByPrice.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
+import { useState } from 'react';
 
-FilterByPrice.propTypes = {};
+FilterByPrice.propTypes = {
+  onChange: PropTypes.func,
+};
 
-function FilterByPrice(props) {
-  return <div>Filter by price</div>;
+function FilterByPrice({ onChange }) {
+  const [values, setValues] = useState({
+    salePrice_gte: 0,
+    salePrice_lte: 0,
+  });
+
+  const handleSubmit = () => {
+    if (onChange) onChange(values);
+  };
+
+  const handleOnChange = (event) => {
+    // Cách 1:
+    // event.persist();
+    // Cách 2:
+    const { name, value } = event.target;
+
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Box>
+      <Typography variant='subtitle2'>GIÁ</Typography>
+
+      <Box>
+        <TextField name='salePrice_gte' value={values.salePrice_gte} onChange={handleOnChange} />
+        <span>-</span>
+        <TextField name='salePrice_lte' value={values.salePrice_lte} onChange={handleOnChange} />
+      </Box>
+
+      <Button variant='outlined' color='primary' onClick={handleSubmit}>
+        Áp dụng
+      </Button>
+    </Box>
+  );
 }
 
 export default FilterByPrice;
